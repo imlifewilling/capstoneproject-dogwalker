e1424708ade71b70a5c6c34e0d2be2f855d9215b
Luca Liu <lucaliu96@gmail.com>
2022-12-03 15:01:17 +0100

    service page

--- src/Components/Service.js
+++ src/Components/Service.js
@@ -1,9 +1,21 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 const Service = () => {
     return (
         <>
             <h1>Service Page</h1>
+            <Box sx={{display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', height:'100vh'}}>
+                <Box key={'filter'} sx={{border: 'black solid 1px'}}>
+                    <h1>Filter</h1>
+                </Box>
+                <Box key={'service list'} sx={{border: 'black solid 1px'}}>
+                    <h1>Service List</h1>
+                </Box>
+                <Box key={'map'} sx={{border: 'black solid 1px'}}>
+                    <h1>Map</h1>
+                </Box>
+            </Box>
         </>
     );
 };
