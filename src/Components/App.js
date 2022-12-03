940151acd78ee3fdac8ec29f6f1fd0f458ddb7ea
Kendal Enz <kendal.enz@gmail.com>
2022-12-03 07:36:11 -0500

    basic profile page

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,7 +4,7 @@ import Login from './Login';
 import { useSelector, useDispatch } from 'react-redux';
 import { loginWithToken } from '../store';
 import { Link, Routes, Route } from 'react-router-dom';
-
+import User from './User/User';
 
 const App = ()=> {
   const { auth } = useSelector(state => state);
@@ -24,7 +24,11 @@ const App = ()=> {
           <div>
             <nav>
               <Link to='/'>Home</Link>
+              <Link to={`/users/${auth.id}`}>Your Profile</Link>
             </nav>
+            <Routes>
+              <Route path='/users/:id' element={<User />} />
+            </Routes>
           </div>
         )
       }
