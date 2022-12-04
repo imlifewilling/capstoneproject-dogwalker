a0dc46b3ae6e4c9d6e2e62099700ace31c52d324
Kendal Enz <kendal.enz@gmail.com>
2022-12-04 12:10:50 -0500

    fixed typo in auth.js stor file

--- src/store/auth.js
+++ src/store/auth.js
@@ -14,8 +14,7 @@ const auth = (state = { }, action)=> {
 export const logout = ()=> {
   window.localStorage.removeItem('token');
   return { type: 'SET_AUTH', auth: {} };
-};x
-
+};
 
 export const loginWithToken = ()=> { //use toke to login
   return async(dispatch)=> {
@@ -76,15 +75,5 @@ export const editUser = (user, navigate) => {
     navigate(`/users/${response.data.id}`);
   };
 };
-
-export const logwith3rdParty = (userinfo, navigate) => {
-  return async(dispatch) => {
-    const response = await axios.post('/api/auth/login/success', userinfo);
-    console.log(response.data)
-    window.localStorage.setItem('token', response.data);
-    dispatch(loginWithToken());
-    navigate('/');
-  }
-}
 
 export default auth;
