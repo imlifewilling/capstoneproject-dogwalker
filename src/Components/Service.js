760c21ce9765eca9b45da22e3650aa11af50d6c2
Luca Liu <lucaliu96@gmail.com>
2022-12-04 22:27:16 +0100

    need to fix filter functionality

--- src/Components/Service.js
+++ src/Components/Service.js
@@ -2,16 +2,33 @@ import React from "react";
 import { Box, Grid } from "@mui/material";
 import { useSelector } from "react-redux";
 import ServiceCard from "./ServiceCard";
-
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 const Service = () => {
     const { services } = useSelector(state=>state);
+    const serviceName = services.map(service => service.task)
+                                .reduce((acc,value)=>{
+                                    if(!acc.includes(value)){
+                                        acc.push(value);
+                                    }
+                                    return acc;
+                                }, []);
+
+    console.log(serviceName)
 
     return (
         <>
             <Grid container>
                 <Grid item md={2} key={'filter'} sx={{border: 'black solid 1px'}}>
                     <h1>Filter</h1>
+                    <FormGroup>
+                        {serviceName.map(name => {
+                            return <FormControlLabel key={name} control={<Checkbox defaultChecked />} label={name} />;
+                        })}
+                    </FormGroup>
+
                 </Grid>
                 <Grid item md={7} key={'service list'} sx={{border: 'black solid 1px'}}>
                     {
