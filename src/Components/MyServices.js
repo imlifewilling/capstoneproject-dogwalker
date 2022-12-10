579a992e21cab0282e5eff6fa61303bee3aab634
Luca Liu <lucaliu96@gmail.com>
2022-12-10 18:20:09 +0100

    service page started

--- src/Components/MyServices.js
+++ src/Components/MyServices.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 const MyServices = () => {
     const { id } = useParams();
+    const { auth } = useSelector(state=>state);
+
+
     return (
-        <h1>My Services {id}</h1>
+        <h1>My Services {auth?.firstname}</h1>
     )
 };
 
