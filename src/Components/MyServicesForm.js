35a168d81828029838123b7f49f6122bdd332c85
Luca Liu <lucaliu96@gmail.com>
2022-12-13 22:22:41 +0100

    form input

--- src/Components/MyServicesForm.js
+++ src/Components/MyServicesForm.js
@@ -7,6 +7,7 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Chip from '@mui/material/Chip';
+import { TextField } from "@mui/material";
 
 // const ITEM_HEIGHT = 48;
 // const ITEM_PADDING_TOP = 8;
@@ -49,20 +50,40 @@ const MyServicesForm = () => {
           // On autofill we get a stringified value.
           {...input, [event.target.name]: typeof value === 'string' ? value.split(',') : value}
         );
-      };
+    };
+
+    const handleSingleChange = (event) => {
+        const {
+          target: { value },
+        } = event;
+
+        // console.log(event.target.name)
+
+        setInput(
+          // On autofill we get a stringified value.
+          {...input, [event.target.name]: value}
+        );
+    };
+    
 
     const taskList = ['Dog Walking', 'House Sitting', 'Dog Day Care'];
 
-    debugger;
+    const availabilityList = ['Morning', 'Afternoon', 'Evening', 'Before Dark', 'Any Time'];
+
+    const dogSizeList = ['Small', 'Medium', 'Large', 'Giant', 'Any Size'];
+
+
+    console.log(input)
 
     return (
         <>
             <h1>Add a New Service Here</h1>
             <FormControl>
             <Box sx={{ m: '1', width: '300' }}>
-                    <InputLabel>Services</InputLabel>
+                    <InputLabel htmlFor="services">Services</InputLabel>
                     <Select
                     multiple
+                    id='services'
                     name='task'
                     value={input.task}
                     onChange={handleChange}
@@ -82,6 +103,64 @@ const MyServicesForm = () => {
                         key={name}
                         value={name}
                         style={getStyles(name, input.task, theme)}
+                        >
+                        {name}
+                        </MenuItem>
+                    ))}
+                    </Select>
+            </Box>
+            </FormControl>
+
+            <TextField
+                id="price"
+                label="Price $"
+                name='price'
+                multiline
+                maxRows={4}
+                value={input.price}
+                onChange={handleSingleChange}
+            />
+
+            <FormControl fullWidth>
+                <InputLabel id="Dog Size">Service Dog Size</InputLabel>
+                <Select
+                labelId="Dog Size"
+                id="demo-simple-select"
+                value={input.serviceDogsize}
+                name="serviceDogsize"
+                onChange={handleSingleChange}
+                >
+                    {dogSizeList.map(size => {
+                        return (<MenuItem key={size} value={size}>{size}</MenuItem>)
+                    })}
+                </Select>
+            </FormControl>
+
+            <FormControl>
+            <Box sx={{ m: '1', width: '300' }}>
+                    <InputLabel htmlFor="availability">Availability</InputLabel>
+                    <Select
+                    multiple
+                    id='availability'
+                    name='availability'
+                    value={input.availability}
+                    onChange={handleChange}
+                    input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                    renderValue={(selected) => (
+                        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: "0.5" }}>
+                        {selected.map((value) => (
+                            <Chip key={value} label={value} />
+                        ))}
+                        </Box>
+                    )}
+                    // MenuProps={MenuProps}
+                    sx={{width:'500'}}
+                    >
+                    {availabilityList.map((name) => (
+                        <MenuItem
+                        key={name}
+                        value={name}
+                        style={getStyles(name, input.availability, theme)}
                         >
                         {name}
                         </MenuItem>
